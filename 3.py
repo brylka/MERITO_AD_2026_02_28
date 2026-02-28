292545b7246eabf9ac767a17074c8103bd193141
Bartosz Bryniarski <brylka@jelenia.com>
2026-02-28 13:58:48 +0100

    Sobota

--- 3.py
+++ 3.py
@@ -10,7 +10,7 @@ from sklearn import tree
 data = load_iris()
 X, y = data.data, data.target
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.8, random_state=42)
 
 gnb_classifier = GaussianNB()
 gnb_classifier.fit(X_train, y_train)
---
+++ train.py
@@ -0,0 +1,19 @@
+import joblib
+from sklearn.datasets import load_digits
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+
+data = load_digits()
+X, y = data.data, data.target
+
+model = DecisionTreeClassifier(random_state=42)
+model.fit(X, y)
+
+joblib.dump(model, 'model.joblib')
+
+
+
+# print(data.images[3])
+# plt.imshow(data.images[3], cmap='gray')
+# plt.show()
