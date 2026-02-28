ced6840bd91caf2a69ed7110b604b370403eee71
Bartosz Bryniarski <brylka@jelenia.com>
2026-02-28 12:13:08 +0100

    Sobota

--- 2.py
+++ 2.py
@@ -1,16 +1,15 @@
+import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
-from sklearn.tree import DecisionTreeClassifier
 import matplotlib.pyplot as plt
-from sklearn import tree
 
 data = load_iris()
 X, y = data.data, data.target
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
 
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
@@ -24,8 +23,6 @@ for k in k_range:
     knn.fit(X_train_scaled, y_train)
     scores.append(knn.score(X_test_scaled, y_test))
 
-print(scores)
-
 plt.figure(figsize=(10,5))
 plt.plot(k_range, scores, marker='o')
 plt.grid(True)
@@ -33,3 +30,12 @@ plt.xlabel('Wartość k (odpytywana liczba sąsiadów)')
 plt.ylabel('Dokładność')
 plt.title('Dokładność KNN dla różnych wartości k (liczby sąsiadów)')
 plt.show()
+
+optimal_k = k_range[np.argmax(scores)]
+print(f"Optymalne k: {optimal_k}")
+
+knn_classifier = KNeighborsClassifier(n_neighbors=optimal_k)
+knn_classifier.fit(X_train_scaled, y_train)
+y_pred = knn_classifier.predict(X_test_scaled)
+
+print(f"Dokładność KNN: {accuracy_score(y_test, y_pred)}")
