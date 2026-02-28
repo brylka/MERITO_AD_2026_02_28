d34b3093ab3a2e97c80cb08be0409f3910d7de30
Bartosz Bryniarski <brylka@jelenia.com>
2026-02-28 09:42:24 +0100

    Sobota

--- 1.py
+++ 1.py
@@ -1,15 +1,13 @@
 from sklearn.datasets import load_iris
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 
 data = load_iris()
 X, y = data.data, data.target
-#print(X)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-#print(X_test)
 dt_classifier = DecisionTreeClassifier(random_state=42)
 
 dt_classifier.fit(X_train, y_train)
@@ -17,3 +15,5 @@ dt_classifier.fit(X_train, y_train)
 y_pred = dt_classifier.predict(X_test)
 
 print(f"Dokładność: {accuracy_score(y_test, y_pred)}")
+print("Raport klasyfikacji:")
+print(classification_report(y_test, y_pred, target_names=data.target_names))
