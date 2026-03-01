6f257dd8601bc86c41d505f0a88f95e29859ea73
Bartosz Bryniarski <brylka@jelenia.com>
2026-03-01 13:38:49 +0100

    Niedziela

--- g1.py
+++ g1.py
@@ -5,7 +5,10 @@ from google import genai
 load_dotenv()
 client = genai.Client()
 
-response = client.models.generate_content(
-    model="gemini-3-flash-preview", contents="Cześć! Jak się masz?"
-)
-print(response.text)
+
+while True:
+    prompt = input("Ja: ")
+    response = client.models.generate_content(
+        model="gemini-3-flash-preview", contents=prompt
+    )
+    print("G:", response.text)
