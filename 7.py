724aa538cea82eb4483b0bf79afff47cf4b3f7e2
Bartosz Bryniarski <brylka@jelenia.com>
2026-03-01 10:52:56 +0100

    Niedziela

---
+++ 7.py
@@ -0,0 +1,30 @@
+from sklearn.cluster import KMeans
+from sklearn.datasets import make_blobs, make_moons
+from sklearn.metrics import  silhouette_score
+import matplotlib.pyplot as plt
+
+#X_blobs, y_true = make_blobs(n_samples=1000, centers=4, cluster_std=1.5, random_state=42)
+X_blobs, y_true = make_moons(n_samples=300, noise=0.05, random_state=42)
+
+kmeans = KMeans(n_clusters=2, random_state=42)
+y_kmeans = kmeans.fit_predict(X_blobs)
+
+
+plt.figure(figsize=(16, 5))
+plt.subplot(1,2,1)
+plt.scatter(X_blobs[:, 0], X_blobs[:, 1], c=y_true)
+plt.title('Prawdziwe etykiety')
+plt.xlabel('Cecha 1')
+plt.ylabel('Cecha 2')
+# plt.show()
+#
+# plt.figure(figsize=(8, 5))
+plt.subplot(1,2,2)
+plt.scatter(X_blobs[:, 0], X_blobs[:, 1], c=y_kmeans)
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1],
+            marker='X', c='red', s=200, label='Centroidy')
+plt.title('Klasteryzacja K-means')
+plt.xlabel('Cecha 1')
+plt.ylabel('Cecha 2')
+plt.legend()
+plt.show()
