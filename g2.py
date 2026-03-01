9f836763d603b4bc3831daa5055ab846e8e11262
Bartosz Bryniarski <brylka@jelenia.com>
2026-03-01 14:33:48 +0100

    Niedziela

--- g2.py
+++ g2.py
@@ -1,25 +1,27 @@
 from dotenv import load_dotenv
 from flask import Flask, request, render_template
 from google import genai
+import json
 
 load_dotenv()
 client = genai.Client()
 app = Flask(__name__)
 
-history = []
+
 
 @app.route("/", methods=["GET", "POST"])
 def chat():
+    history = []
     if request.method == "POST":
+        history = json.loads(request.form.get("history", "[]"))
         prompt = request.form["prompt"]
         history.append({"role": "user", "parts": [{"text": prompt}]})
         response = client.models.generate_content(
             model="gemini-3-flash-preview", contents=history
         )
-        asistant_text = response.text
-        history.append({"role": "model", "parts": [{"text": asistant_text}]})
+        history.append({"role": "model", "parts": [{"text": response.text}]})
 
-    return render_template("chat.html", history=history)
+    return render_template("chat.html", history=history, history_json=json.dumps(history))
 
 if __name__ == '__main__':
     app.run(debug=True)
