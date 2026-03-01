06a9af95973c0303011f82ca74d1b02c303ab89d
Bartosz Bryniarski <brylka@jelenia.com>
2026-03-01 13:45:14 +0100

    Niedziela

--- g1.py
+++ g1.py
@@ -5,10 +5,14 @@ from google import genai
 load_dotenv()
 client = genai.Client()
 
+history = []
 
 while True:
     prompt = input("Ja: ")
+    history.append({"role": "user", "parts": [{"text": prompt}]})
     response = client.models.generate_content(
-        model="gemini-3-flash-preview", contents=prompt
+        model="gemini-3-flash-preview", contents=history
     )
-    print("G:", response.text)
+    asistant_text = response.text
+    history.append({"role": "model", "parts": [{"text": asistant_text}]})
+    print("G:", asistant_text)
